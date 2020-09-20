1e8f1c7c70a0f07fd2797a447385f50786cc4411
venkatgiri <venkatgiri@192.168.43.117>
2020-09-20 13:26:42 +0530

    added a new one

---
+++ Java3.0/src/Jenkins/Learndevops.java
@@ -0,0 +1,10 @@
+package Jenkins;
+
+public class Learndevops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("lets explore devops models");
+	}
+
+}
